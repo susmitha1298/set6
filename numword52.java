ff079fe6a4b3c1a30f783136f162d8bc73434e1f
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-08-27 10:45:41 +0530

    Create numword52.java

---
+++ numword52.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+		String ones[] = {" ", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine", " ten"};
+		int n;
+		n=sc.nextInt();
+		{
+			if(n<=10)
+			System.out.print(ones[n]);	// your code goes here
+	}
+}
+}
