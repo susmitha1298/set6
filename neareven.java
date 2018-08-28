c6c8a319a2ac09f74c562e8039524a6ff7ce483c
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-08-28 09:44:35 +0530

    Create neareven.java

---
+++ neareven.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int even=0;
+		if(n%2==0)
+		{
+			even=n;
+		}
+		else
+		{
+			even=n-1;
+		}
+		System.out.print(even);
+	}
+}
