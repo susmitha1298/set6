59e7158456d215700ecace98820158324b21e193
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-08-28 11:41:38 +0530

    Create natural60.java

---
+++ natural60.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0,i;
+		for(i=0;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
